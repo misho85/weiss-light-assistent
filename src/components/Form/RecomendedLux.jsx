25d0a3259e6caba335893878aedd987657a307a5
M150 <stevovic@gmail.com>
2020-08-27 23:19:48 +0200

    update RecomendedLux

--- src/components/Form/RecomendedLux.jsx
+++ src/components/Form/RecomendedLux.jsx
@@ -1,28 +1,46 @@
-import React, { memo } from 'react';
+import React, { useState, memo } from 'react';
 import styled from 'styled-components';
 import { MagnifyingGlass } from '../../icons';
 
 const Wrapper = styled.div`
+  position: relative;
+  transition: all 200ms;
+  height: 2.4em;
+  width: ${p => (p.active ? `14em` : `2.4em`)};
+  border-radius: 0.4em;
+  background-color: ${p => p.theme.colors.gray};
+`;
+
+const Search = styled.button`
+  position: absolute;
+  left: 0;
+  width: 2.4em;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  transition: all 200ms;
-  height: 2.4em;
-  width: 2.4em;
-  border-radius: 0.4em;
-  background-color: ${p => p.theme.colors.gray};
-  cursor: pointer;
 
-  svg {
+  > svg {
     width: 1.8em;
     height: 1.8em;
   }
 `;
 
-const RecomendedLux = () => (
-  <Wrapper>
-    <MagnifyingGlass />
-  </Wrapper>
-);
+const RecomendedLux = () => {
+  const [active, setActive] = useState(false);
+
+  const toggleActive = () => setActive(!active);
+
+  // napraviti outsideClick umesto blur
+  const disableActive = () => setActive(false);
+
+  return (
+    <Wrapper active={active} onBlur={disableActive}>
+      <Search onClick={toggleActive}>
+        <MagnifyingGlass />
+      </Search>
+    </Wrapper>
+  );
+};
 
 export default memo(RecomendedLux);
